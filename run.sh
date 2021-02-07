36d313aa65cb7f43996ec24c53e2dd6ae41c47f4
89528 <am4919566@gmail.com>
2021-02-07 22:44:29 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup java -jar  -DlogPath=/usr/local/tomcat/logs/ -Dport=8080 -Duser.timezone="Asia/Kolkata" /var/install/zaakpay/zaak-self-onboarding/ROOT.war > /usr/local/tomcat/logs/console.log &
+nohup java -jar  -DlogPath=/usr/local/tomcat/logs/ -Dport=8080 -Duser.timezone="Asia/Kolkata" /usr/local/tomcat/webapps/ROOT.war > /usr/local/tomcat/logs/console.log &
