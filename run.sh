075d56a1b69cc156e1d6632f2eccc995c66ee191
89528 <am4919566@gmail.com>
2021-02-07 22:43:26 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/local/tomcat/bin/catalina.sh run
+nohup java -jar  -DlogPath=/usr/local/tomcat/logs/ -Dport=8080 -Duser.timezone="Asia/Kolkata" /var/install/zaakpay/zaak-self-onboarding/ROOT.war > /usr/local/tomcat/logs/console.log &
