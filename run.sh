b82159c9c1afdc332aef848febeed2cb27df09b3
89528 <am4919566@gmail.com>
2021-02-08 00:47:58 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup java -jar  -DlogPath=/usr/local/tomcat/logs/ -Dport=8080 -Duser.timezone="Asia/Kolkata" /usr/local/tomcat/webapps/demo-0.0.1-SNAPSHOT.jar > /usr/local/tomcat/logs/console.log &
+nohup java -jar  -DlogPath=/usr/local/tomcat/logs/ -Dport=8080 -Duser.timezone="Asia/Kolkata" /usr/local/tomcat/webapps/Root.jar > /usr/local/tomcat/logs/console.log &
